c7a7372b987e71c74b05449aba5146590408bcb0
John McLaughlin <jmclaughlin@isg.cs.uni-magdeburg.de>
2016-03-15 15:36:08 +0100

    - changed to OpenGL 4.5 (0.9 way)

--- RecirculationSurfaces/src/main.cpp
+++ RecirculationSurfaces/src/main.cpp
@@ -3,7 +3,10 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	// setup opengl 
+	ofGLWindowSettings settings;
+	settings.setGLVersion(4, 5);
+	ofCreateWindow(settings);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
